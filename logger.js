636b2d618a0cfff834623b5991ad662d3f721d61
Chris Kozlowski <kozlowskicd@gmail.com>
2019-06-06 21:55:58 -0700

    finish tasks and add readme

--- logger.js
+++ logger.js
@@ -6,34 +6,32 @@ const files = new QClient('files');
 const database = new QClient('database');
 
 files.subscribe('file-save', payload => {
-  console.log(`File Save: ${payload}`);
+  console.log('File Save: ', payload)
 });
 
 files.subscribe('file-error', payload => {
-  console.log(`File Error: ${payload}`);
+  console.log('File Error: ', payload);
 });
 
 database.subscribe('create', payload => {
-  console.log(`Create event: ${payload}`);
+  console.log('Create event: ', payload);
 });
 
 database.subscribe('read', payload => {
-  console.log(`Read event: ${payload}`);
+  console.log('Read event: ', payload);
 });
 
 database.subscribe('update', payload => {
-  console.log(`Update event: ${payload}`);
+  console.log('Update event: ', payload);
 });
 
 database.subscribe('delete', payload => {
-  console.log(`Delete event: ${payload}`);
+  console.log('Delete event: ', payload);
 });
 
 database.subscribe('error', payload => {
-  console.log(`Error event: ${payload}`);
+  console.log('Error event: ', payload);
 });
 
 console.log(files.subscriptions());
 console.log(database.subscriptions());
-
-require('./app/app.js');
